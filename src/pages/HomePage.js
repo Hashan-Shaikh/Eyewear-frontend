d73eaa4bbdba6dc0e446f558c5b33fa28968baf9
Affan Siddiqui <unifyai>
2023-04-30 23:21:06 +0500

    Added ProductPg and Cart in Pages folder

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,12 +2,19 @@ import Slider from "../UI/Slider";
 import NavBar from "../UI/NavBar";
 import Products from "../UI/Products";
 import Newsletter from "../UI/Newsletter";
+import ProductPg from "./ProductPg";
+import Cart from "./Cart";
+
 const HomePage = () => {
     return (<>
         <NavBar />
         <Slider />
         <Products />
         <Newsletter />
+
+        {/* <ProductPg />
+        <Cart/> */}
+        
     </>)
 }
 export default HomePage;
